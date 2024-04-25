45d3d5e2cb44408918b9d6f06e4cba067a2394a5
kavya-ang <61929764+kavya-ang@users.noreply.github.com>
2024-04-25 14:57:48 +0000

    operations done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -28,3 +28,35 @@ let isLoggedIn = "Kavya"
  console.log(stringNumber);
  console.log(typeof stringNumber);
 
+// ------------------Operations ----------------------------//
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+//console.log(2+2);
+
+let str1 = "Kavya"
+let str2 = "Vijay"
+
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1+2+ "2");
+
+console.log((3+4) * 5 % 3);
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++; //or ++gameCounter
+console.log(gameCounter);
+
