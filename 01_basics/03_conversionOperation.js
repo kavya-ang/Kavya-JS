2df9c8f3e9c7f6eeeb35afbfb02998c24cc54e15
kavya-ang <61929764+kavya-ang@users.noreply.github.com>
2024-04-25 14:40:58 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = 33
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0;
+// null => 0
+//undeined => NaN
+
+
+let isLoggedIn = "Kavya"
+ let booleanIsLoggedIn = Boolean(isLoggedIn)
+ console.log(booleanIsLoggedIn);
+
+ // 1 => true; 0 => false
+ // "" => false
+ // "Kavya" => true
+
+ let someNumber = 33
+
+ let stringNumber = String(someNumber)
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
+
